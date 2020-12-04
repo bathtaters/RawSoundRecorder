303a1de1e3a7e95ca4eff1efa060166e8399a682
bathtaters <nomoremrnice7@gmail.com>
2020-12-04 01:00:11 -0500

    Initial Commit

---
+++ RawSoundRecorder/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  RawSoundRecorder
+//
+//  Created by Nick Chirumbolo on 12/4/20.
+//  Copyright © 2020 Nice Sound. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
